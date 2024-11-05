2ff5fe494dbec698f8fe73c274a9c8041a78ccc1
Douglas <dengratulis@gmail.com>
2024-11-05 19:59:41 -0300

    Feito struct pra representa as vendas

--- main.c
+++ main.c
@@ -3,6 +3,14 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    typedef struct {
+    int idVenda;
+    int idProd;
+    char nomeProd[50];
+    int qtdVendida;
+    float precoTotal;
+    int dia, mes, ano;
+} venda;   // feito pra representa a venda (vinicius)
+
     return 0;
 }
