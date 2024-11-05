94222c704f3cb502ff6f51c9d174d8634f4deebc
Douglas <dengratulis@gmail.com>
2024-11-05 20:15:53 -0300

    deu certo

--- main.c
+++ main.c
@@ -11,7 +11,7 @@
     } venda;   // feito pra representa a venda (vinicius)
 
 #define maximoDeVendas 10000
-    Venda venda[maximoDeVendas]
+    venda vendas[maximoDeVendas];
     int totalVendas = 0; // tamanho do array de struct venda
 
 int main() {
@@ -21,9 +21,9 @@ printf("===MENU DO FINANCEIRO===\n");
 
 
     void carregarArquivoVendas () { // aqui eu vou precisar de uma funcao void quer carregue o arquivo contendo as informacoes da venda.
-        FILE arquivo* = fopen("C:\Users\dotul\Projetos em C\Financeiro\bin\Debug\Vendas.txt", "r"); // vo acessa pra ler o arquivo de vendas
+        FILE *arquivo = fopen("C:\\Users\\dotul\\Projetos em C\\Financeiro\\bin\\Debug\\Vendas.txt", "r"); // vo acessa pra ler o arquivo de vendas
         if (arquivo == NULL) {
-            printf("Arquivo não encontrado.") // programacao defensiva
+            printf("Arquivo não encontrado."); // programacao defensiva
          totalVendas = 0; // comecando com 0 pra comecar a percorrer na leitura
          while (fscanf(arquivo, "%d,%d,%49[^,],%d,%f,%d,%d,%d",
                   &vendas[totalVendas].idVenda,
@@ -33,7 +33,7 @@ printf("===MENU DO FINANCEIRO===\n");
                   &vendas[totalVendas].precoTotal,
                   &vendas[totalVendas].dia,
                   &vendas[totalVendas].mes,
-                  &vendas[totalVendas].ano) == 7) {
+                  &vendas[totalVendas].ano) == 8) {
         totalVendas++;
     }
         }
