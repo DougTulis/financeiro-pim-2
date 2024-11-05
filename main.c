4424aa6b325c035e26238246a83aff78cec9e8c9
Douglas <dengratulis@gmail.com>
2024-11-05 20:23:45 -0300

    Criando uma funcao pra faezr um relatorio de receitas das vendas

--- main.c
+++ main.c
@@ -1,41 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
- typedef struct {
-        int idVenda;
-        int idProd;
-        char nomeProd[50];
-        int qtdVendida;
-        float precoTotal;
-        int dia, mes, ano;
-    } venda;   // feito pra representa a venda (vinicius)
+typedef struct {
+    int idVenda;
+    int idProd;
+    char nomeProd[50];
+    int qtdVendida;
+    float precoTotal;
+    int dia, mes, ano;
+} venda;   // feito pra representa a venda (vinicius)
 
 #define maximoDeVendas 10000
-    venda vendas[maximoDeVendas];
-    int totalVendas = 0; // tamanho do array de struct venda
+venda vendas[maximoDeVendas];
+int totalVendas = 0; // tamanho do array de struct venda
 
 int main() {
-printf("===MENU DO FINANCEIRO===\n");
+    printf("===MENU DO FINANCEIRO===\n");
     return 0;
 }
 
 
-    void carregarArquivoVendas () { // aqui eu vou precisar de uma funcao void quer carregue o arquivo contendo as informacoes da venda.
-        FILE *arquivo = fopen("C:\\Users\\dotul\\Projetos em C\\Financeiro\\bin\\Debug\\Vendas.txt", "r"); // vo acessa pra ler o arquivo de vendas
-        if (arquivo == NULL) {
-            printf("Arquivo não encontrado."); // programacao defensiva
-         totalVendas = 0; // comecando com 0 pra comecar a percorrer na leitura
-         while (fscanf(arquivo, "%d,%d,%49[^,],%d,%f,%d,%d,%d",
-                  &vendas[totalVendas].idVenda,
-                  &vendas[totalVendas].idProd,
-                  vendas[totalVendas].nomeProd,
-                  &vendas[totalVendas].qtdVendida,
-                  &vendas[totalVendas].precoTotal,
-                  &vendas[totalVendas].dia,
-                  &vendas[totalVendas].mes,
-                  &vendas[totalVendas].ano) == 8) {
-        totalVendas++;
-    }
+void carregarArquivoVendas () { // aqui eu vou precisar de uma funcao void quer carregue o arquivo contendo as informacoes da venda.
+    FILE *arquivo = fopen("C:\\Users\\dotul\\Projetos em C\\Financeiro\\bin\\Debug\\Vendas.txt", "r"); // vo acessa pra ler o arquivo de vendas
+    if (arquivo == NULL) {
+        printf("Arquivo não encontrado."); // programacao defensiva
+        totalVendas = 0; // comecando com 0 pra comecar a percorrer na leitura
+        while (fscanf(arquivo, "%d,%d,%49[^,],%d,%f,%d,%d,%d",
+                      &vendas[totalVendas].idVenda,
+                      &vendas[totalVendas].idProd,
+                      vendas[totalVendas].nomeProd,
+                      &vendas[totalVendas].qtdVendida,
+                      &vendas[totalVendas].precoTotal,
+                      &vendas[totalVendas].dia,
+                      &vendas[totalVendas].mes,
+                      &vendas[totalVendas].ano) == 8) {
+            totalVendas++;
         }
+        fclose(arquivo);
+    }
+
+    float CalculoDeReceita() {
 
     }
+
+}
