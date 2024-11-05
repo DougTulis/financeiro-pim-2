921d67df1689288714e4b42391188b20bac9f397
Douglas <dengratulis@gmail.com>
2024-11-05 20:11:33 -0300

    Arrumado escopo das funcoes e depois feito uma estrutura de leitura pra carregar o arquivo e ler ele

--- main.c
+++ main.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    typedef struct {
-    int idVenda;
-    int idProd;
-    char nomeProd[50];
-    int qtdVendida;
-    float precoTotal;
-    int dia, mes, ano;
-} venda;   // feito pra representa a venda (vinicius)
-
+ typedef struct {
+        int idVenda;
+        int idProd;
+        char nomeProd[50];
+        int qtdVendida;
+        float precoTotal;
+        int dia, mes, ano;
+    } venda;   // feito pra representa a venda (vinicius)
 
 #define maximoDeVendas 10000
-Venda vendas[maximoDeVendas]
-int totalVendas = 0; // tamanho do array de struct vendas
+    Venda vendas[maximoDeVendas]
+    int totalVendas = 0; // tamanho do array de struct venda
 
-void carregarArquivoVendas () { // aqui eu vou precisar de uma funcao void quer carregue o arquivo contendo as informacoes da venda.
-
-}
-
+int main() {
+printf("===MENU DO FINANCEIRO===\n");
     return 0;
 }
+
+
+    void carregarArquivoVendas () { // aqui eu vou precisar de uma funcao void quer carregue o arquivo contendo as informacoes da venda.
+        FILE arquivo* = fopen("C:\Users\dotul\Projetos em C\Financeiro\bin\Debug\Vendas.txt", "r"); // vo acessa pra ler o arquivo de vendas
+        if (arquivo == NULL) {
+            printf("Arquivo não encontrado.") // programacao defensiva
+        }
+
+    }
