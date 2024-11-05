ca00744fab6757632a842f760a8716ce18361986
Douglas <dengratulis@gmail.com>
2024-11-05 20:13:53 -0300

    Teste de leitura

--- main.c
+++ main.c
@@ -11,7 +11,7 @@
     } venda;   // feito pra representa a venda (vinicius)
 
 #define maximoDeVendas 10000
-    Venda vendas[maximoDeVendas]
+    Venda venda[maximoDeVendas]
     int totalVendas = 0; // tamanho do array de struct venda
 
 int main() {
@@ -24,6 +24,18 @@ printf("===MENU DO FINANCEIRO===\n");
         FILE arquivo* = fopen("C:\Users\dotul\Projetos em C\Financeiro\bin\Debug\Vendas.txt", "r"); // vo acessa pra ler o arquivo de vendas
         if (arquivo == NULL) {
             printf("Arquivo não encontrado.") // programacao defensiva
+         totalVendas = 0; // comecando com 0 pra comecar a percorrer na leitura
+         while (fscanf(arquivo, "%d,%d,%49[^,],%d,%f,%d,%d,%d",
+                  &vendas[totalVendas].idVenda,
+                  &vendas[totalVendas].idProd,
+                  vendas[totalVendas].nomeProd,
+                  &vendas[totalVendas].qtdVendida,
+                  &vendas[totalVendas].precoTotal,
+                  &vendas[totalVendas].dia,
+                  &vendas[totalVendas].mes,
+                  &vendas[totalVendas].ano) == 7) {
+        totalVendas++;
+    }
         }
 
     }
