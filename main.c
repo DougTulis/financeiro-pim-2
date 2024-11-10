c57b26d0f8e0b2e3292db19259e94cc53e371603
Douglas Engratulis <dengratulis@gmail.com>
2024-11-10 17:54:50 -0300

    Procedente...

--- main.c
+++ main.c
@@ -13,25 +13,31 @@ typedef struct {
 #define maximoDeVendas 10000
 venda vendas[maximoDeVendas];
 int totalVendas = 0; // tamanho do array de struct venda
+void carregarArquivoVendas();
+float calculoDeReceita();
 
 int main() {
+     carregarArquivoVendas();
     int opcao;
-       printf("\n=== MENU DO FINANCEIRO ===\n");
-        printf("1. Ver Receita Total\n");
-        printf("2. Exibir Resumo de Receitas\n");
-        printf("0. Sair\n");
-        printf("Escolha uma opção: ");
-        scanf("%d", &opcao);
+    printf("\n=== MENU DO FINANCEIRO ===\n");
+    printf("1. Ver Receita Total\n");
+    printf("2. Exibir Resumo de Receitas\n");
+    printf("0. Sair\n");
+    printf("Escolha uma opção: ");
+    scanf("%d", &opcao);
 
     switch(opcao) {
-    case 1:
+    case 1: {
+        float totalReceita = calculoDeReceita(); // total vai recebe o calculo total receita vinda da funcao que realiza toda a soma
+        printf("Receita Total: R$ %.2f\n", totalReceita);
+        break;
+    }
     case 2:
-
-    case 3:
-
-    case 4:
+        // exibirResumoReceitas(); em manutencao
+        break;
+    case 0:
         printf("Saindo do sistema financeiro...\n");
-                break;
+        break;
     default:
         printf("Opção invalida...");
         system("pause");
@@ -39,8 +45,6 @@ int main() {
 
     return 0;
 }
-
-
 
 void carregarArquivoVendas () { // aqui eu vou precisar de uma funcao void quer carregue o arquivo contendo as informacoes da venda.
     FILE *arquivo = fopen("E:\\Linguagem_C\\financeiro-pim-2\\bin\\Debug\\Vendas.txt", "r"); // vo acessa pra ler o arquivo de vendas
@@ -60,15 +64,12 @@ void carregarArquivoVendas () { // aqui eu vou precisar de uma funcao void quer 
         }
         fclose(arquivo);
     }
+}
 
-    float CalculoDeReceita() {
-        float receita;
-
-        for (int i = 0 ; i < totalVendas ; i ++) {
-            receita += vendas[i].precoTotal;
-            printf("DOUGLASS");
-        }
-        return receita;
+   float calculoDeReceita() {
+    float receita = 0;
+    for (int i = 0; i < totalVendas; i++) {
+        receita += vendas[i].precoTotal;
     }
-
+    return receita;
 }
